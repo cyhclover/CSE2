a62dfb521e477656c5ba2404eeb2e5d585bbe767
cyhclover <yuc315@lehigh.edu>
2014-08-27 16:51:01 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//////////////////
+//Yuhua Chen
+//LIN: 819533398
+//hw01
+//WelcomeClass
+
+
+public class WelcomeClass{
+    
+        public static void main(String[] args) {
+            //print the statement
+            System.out.println("   -----------");
+            System.out.println("   | WELCOME |");
+            System.out.println("   -----------");
+            System.out.println("   ^  ^  ^  ^  ^  ^");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+            System.out.println("|-Y--U--C--3--1--5-|");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+            System.out.println("  v  v  v  v  v  v");
+            System.out.println("This summer, I stayed at Lehigh for my internship.");
+            System.out.println("It was a lot of fun");
+            
+            //end of program
+        }
+}
