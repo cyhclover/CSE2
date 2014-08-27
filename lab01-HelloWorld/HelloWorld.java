aa846017ef88b7eab6b81567a80c0cbf2b89e76b
cyhclover <yuc315@lehigh.edu>
2014-08-27 16:24:58 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//////////////////
+//Yuhua Chen
+//Demo 0
+//Lab1
+// HelloWorld
+
+
+
+
+public class HelloWorld{
+    
+        public static void main(String[] args) {
+            //print the statement
+            System.out.println("#######################");
+            System.out.println("#######################");
+            System.out.println("#                     #");
+            System.out.println("#  Hello World 2014!  #");
+            System.out.println("#                     #");
+            System.out.println("#######################");
+            System.out.println("#######################");
+            System.out.println("test");
+            
+            //end of program
+        }
+}
